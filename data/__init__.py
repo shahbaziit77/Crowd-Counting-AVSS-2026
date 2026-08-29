5f42bd973c613863de12b068c9b6add5adbd8089
Shahbaz Ahmad <shahbazitm777@gmail.com>
2026-08-29 15:30:15 +0530

    Update __init__.py

--- data/__init__.py
+++ data/__init__.py
@@ -1,6 +1,6 @@
-from .CSRNet_dataset import CrowdCountingDataset2, data_path_gen_SHT
-from .CSRNet_dataset import CrowdCountingDataset2, data_path_gen_UCF_CC_50
-from .CSRNet_dataset import CrowdCountingDataset2, data_path_gen_UCF_QNRF
+from .CSRNet_dataset import CrowdCountingDataset2, data_path_gen_SHT          # Testing: ShanghaiTech Part_A/Part_B
+from .CSRNet_dataset import CrowdCountingDataset2, data_path_gen_UCF_CC_50    # Testing: UCF_CC_50
+from .CSRNet_dataset import CrowdCountingDataset2, data_path_gen_UCF_QNRF     # Testing: UCF-QNRF
 
-from .CSRNet_dataset import CrowdCountingDataset_v3, CrowdCountingDataset_v3_test, data_path_gen_SHT  
-from .CSRNet_dataset import CrowdCountingDataset_v3, CrowdCountingDataset_v3_test, data_path_gen_UCF_QNRF  
+from .CSRNet_dataset import CrowdCountingDataset_v3, CrowdCountingDataset_v3_test, data_path_gen_SHT         # Training: ShanghaiTech Part_A (SHT_A)
+from .CSRNet_dataset import CrowdCountingDataset_v3, CrowdCountingDataset_v3_test, data_path_gen_UCF_QNRF    # Training: UCF-QNRF
