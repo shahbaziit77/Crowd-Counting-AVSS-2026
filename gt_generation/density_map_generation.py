aa2eb50b38cb2197854d84c3a2e896be49e1d57d
Shahbaz Ahmad <shahbazitm777@gmail.com>
2026-09-03 16:33:40 +0530

    Update density_map_generation.py

--- gt_generation/density_map_generation.py
+++ gt_generation/density_map_generation.py
@@ -16,7 +16,9 @@ from image import *
 
 #--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
+#-------------------------------------------------------------------------------------------------------------------------------------------
 # Density map generation: Create a function to generate the density maps using a geometry-adaptive kernel (GAK) for the images
+#-------------------------------------------------------------------------------------------------------------------------------------------
 def gaussian_filter_density(gt):                           
     print(gt.shape)                           
     density = np.zeros(gt.shape, dtype=np.float32)          
@@ -49,9 +51,11 @@ def gaussian_filter_density(gt):
     print('done.')                           
     return density 
 
-#=====================================================================================================================================================
+#===========================================================================================================================================================
 
-# Density map generation: Create a function to generate the density maps using a fixed gaussian kernel (FGK) for the images
+#--------------------------------------------------------------------------------------------------------------------------------------------
+# Density map generation: Create a function to generate the density maps using a fixed Gaussian kernel (FGK) for the images
+#--------------------------------------------------------------------------------------------------------------------------------------------
 def generate_density_map_fixed_gaussian_kernel(img, points, kernel_size=15, sigma=10.0):
     '''
     Generate ground truth (GT) density map for crowd counting.
